7693e948774029d7c2fe705dd53661dbd6486614
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-16 11:41:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Code your solution in this file!
 
 function logDriverNames(col) {
-  col.forEach(function (el, i, arr) { console.log(el) })
+  col.forEach(function (el, i, arr) {
+    console.log(el)
+   })
 }
